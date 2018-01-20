c1d5dc0b7888fa7e36a869b8da6ba5b3f70060f3
“00pf00 <408445889@qq.com>
2018-01-21 03:41:23 +0800

    修改类名

--- register/src/main/java/registApplication/Register.java
+++ register/src/main/java/registApplication/Register.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Regist {
+public class Register {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Regist.class, args);
+		SpringApplication.run(Register.class, args);
 
 	}
 
